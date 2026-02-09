952cab84a6fb97c20b79e879d6e35d2f6a208968
Vic <victorlymar@gmail.com>
2026-02-09 19:25:33 +0200

    add max value

--- utils/random/get_numbers_ticket.py
+++ utils/random/get_numbers_ticket.py
@@ -1,9 +1,11 @@
 import random
 
 
+MAX_NUMBER = 1000
+
 #Return a list of randomly selected unique numbers
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
-    if min >= max or quantity <= 0 or quantity > max - min + 1:
+    if min >= max or max > MAX_NUMBER or quantity <= 0 or quantity > max - min + 1:
         return []
     
     numbers = random.sample(range(min, max + 1), quantity)
@@ -13,7 +15,7 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
 def main():
     try:
         min_input = int(input("Enter the minimum number: "))
-        max_input = int(input("Enter the maximum number: "))
+        max_input = int(input(f"Enter the maximum number (max {MAX_NUMBER}): "))
         quantity_input = int(input("Enter the quantity of numbers to generate: "))
 
         result = get_numbers_ticket(min_input, max_input, quantity_input)
